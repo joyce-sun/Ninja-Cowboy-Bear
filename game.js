060d74c965baaf5751dec7b01601af483aa4b71a
Joyce Sun <joyce-sun@outlook.com>
2023-03-01 15:49:28 -0500

    finish playRound function

---
+++ game.js
@@ -0,0 +1,34 @@
+function getComputerChoice() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    let rand = Math.random() * 2;
+    return choice[Math.round(rand)];
+}
+
+function playRound(playerSelection, computerSelection) { 
+    let ps = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if ((ps === 'Rock' && computerSelection === 'Scissors') || (ps === 'Paper' && computerSelection ===  'Rock') || (ps === 'Scissors' && computerSelection === 'Paper')) {
+        return true;
+    }
+    else if ((ps === 'Rock' && computerSelection === 'Paper') || (ps === 'Paper' && computerSelection ===  'Scissors') || (ps === 'Scissors' && computerSelection === 'Rock')) {
+        return false;
+    }
+    else {
+        return 'You Tie!'
+    }
+}
+
+function game() {
+    let playerSelection = prompt("Make your selection of Rock, Paper or Scissors:");
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0, i < 5; i++) {
+        if (playRound(playerSelection, getComputerChoice()) === true) {
+            playerScore++;
+        }
+        else if (playRound(playerSelection, getComputerChoice()) === false) {
+            computerScore++;
+        }
+        console.log(playerScore + ":" + computerScore);
+    }
+}
+
